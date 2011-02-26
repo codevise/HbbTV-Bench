a078313cbe0f9327eef2451b743966dcc8cf8595
Gregor Weckbecker <gregor@feiner-fug.net>
2011-02-26 16:39:45 +0100

    added jquery and made results tables sortable

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,8 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
+
+$(function () { 
+    $("#test-runs").tablesorter()
+        .bind("sortEnd",function() {
+	    $("tr:odd").addClass("odd").removeClass("even");
+            $("tr:even").addClass("even").removeClass("odd");
+	});;
+});
